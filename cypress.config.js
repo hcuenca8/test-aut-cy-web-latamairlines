3bab4cdf154d562e12e983cc54fb53f60a67242f
hcuenca8 <89863350+hcuenca8@users.noreply.github.com>
2023-11-28 08:59:03 -0500

    Update cypress.config.js
    :recycle: add retry

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,14 @@ module.exports = defineConfig({
     baseUrl: 'https://www.latamairlines.com',
     chromeWebSecurity: false,
     uncaughtException: false,
-
+    
+    retries: {
+      // retry cypress run
+      runMode: 3,
+      // retry cypress open
+      openMode: 3
+    },
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
       allureWriter(on, config);
